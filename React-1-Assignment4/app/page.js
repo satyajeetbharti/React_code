ceb638775d1fafcaaf67295b985c8c9148db591d
satyajeet bharti <satyajeetbhartirahul@gmail.com>
2023-10-06 02:19:37 +0530

    added react assignment4

---
+++ React-1-Assignment4/app/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p>Funny pic 1</p>
+      <img src="https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg?fit=700%2C500?fit=700,700"></img>
+
+      <p>Funny pic 2</p>
+      <div>
+        <img src="https://media.istockphoto.com/id/476924884/photo/naptime-office-worker.jpg?s=612x612&w=0&k=20&c=fk312ObLJ3RVSr8o8TySY7VSllpKWx182rNCKvvJkkM="></img>
+      </div>
+
+      <div>
+        <p>Funny Pic 3</p>
+        <img src="https://media.istockphoto.com/id/696178594/vector/rise-of-the-robots.jpg?s=612x612&w=0&k=20&c=C1z9fsUDn32v4C76VIHD94g917QX0IVmKWs39qZX1VQ="></img>
+      </div>
+    </div>
+  )
+}
+
+export default page
+
